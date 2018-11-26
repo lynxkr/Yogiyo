1b9c5a1baf7bb549ce6f18f451ffd2baf807a2f3
kira1021 <dldbdjq@gmail.com>
2018-11-26 11:13:56 +0900

    작업 공간 설정 ㅋ

---
+++ Yogiyo/Up/ViewController/MenuViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MenuViewController.swift
+//  Yogiyo
+//
+//  Created by Kira on 26/11/2018.
+//  Copyright © 2018 EunKyung Park. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+        
+    }
+    
+
+}
