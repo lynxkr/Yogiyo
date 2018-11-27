1814c8bcd7ed42f640e5c8b5a26c1bb2a39f5a5a
ekaypark <ek.eunkyung@gmail.com>
2018-11-27 10:38:23 +0900

    view controllers

---
+++ Yogiyo/Up/ViewController/MainVC.swift
@@ -0,0 +1,43 @@
+//
+//  MainVC.swift
+//  Yogiyo
+//
+//  Created by EunKyung Park on 11/27/18.
+//  Copyright © 2018 EunKyung Park. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    //// 요기요 디폴트 홈page
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(showLogin), name: NSNotification.Name("showLogin"), object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(showCoupon), name: NSNotification.Name("showCoupon"), object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(showReview), name: NSNotification.Name("showReview"), object: nil)
+
+    }
+    
+    @objc func showLogin() {
+        performSegue(withIdentifier: "showLogin", sender: nil)
+    }
+    @objc func showCoupon() {
+        performSegue(withIdentifier: "showCoupon", sender: nil)
+    }
+    @objc func showReview() {
+        performSegue(withIdentifier: "showReview", sender: nil)
+    }
+    
+    
+    //need this for menu
+    @IBAction func onMenuTapped() {
+        print("toggle side menu")
+        NotificationCenter.default.post(name: NSNotification.Name("toggleSideMenu"), object: nil)
+    }
+
+
+}
