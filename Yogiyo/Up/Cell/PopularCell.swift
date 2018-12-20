a11c186636ae1f9e1b3fbba87701991465e151ac
ekaypark <ek.eunkyung@gmail.com>
2018-12-20 16:41:24 +0900

    popular

--- Yogiyo/Up/Cell/PopularCell.swift
+++ Yogiyo/Up/Cell/PopularCell.swift
@@ -4,6 +4,8 @@ import AlamofireImage
 import Foundation
 
 
+
+
 class PopularCell: UICollectionViewCell, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     final let url = URL(string: "https://jogiyo.co.kr/restaurants/api/restaurant/?ordering=-review_avg")
     
@@ -46,16 +48,18 @@ class PopularCell: UICollectionViewCell, UICollectionViewDelegate, UICollectionV
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setup()
-//        downloadJSON()
         testJSON()
-        //        getData()
+        setup()
+
+
     }
+    
+    
     
     
     func testJSON() {
         guard let url = URL(string: "https://jogiyo.co.kr/restaurants/api/restaurant/?ordering=-review_avg") else { return }
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+        let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
         guard let dataResponse = data,
         error == nil else {
         print(error?.localizedDescription ?? "Response Error")
@@ -67,19 +71,23 @@ class PopularCell: UICollectionViewCell, UICollectionViewDelegate, UICollectionV
             guard let jsonArray = jsonResponse as? [[String: Any]] else {
                 return
             }
+            
+            
+            let newArray = jsonArray[..<5]
+            
+            print(newArray.count)
+            
 
-            for dic in jsonArray{
+            for dic in newArray{
                 guard let name = dic["name"] as? String else { return }
                 print(name)
             }
             
-           
             let decoder = JSONDecoder()
             let result = try decoder.decode(Foodlist.self, from:
                 dataResponse)
             self.restaurantData = result
             let arraySlice = self.restaurantData[..<5]
-//            let restList = Array(arraySlice)
             self.restList = Array(arraySlice)
             print(self.restList.count)
             
@@ -90,7 +98,7 @@ class PopularCell: UICollectionViewCell, UICollectionViewDelegate, UICollectionV
         } catch let parsingError {
         print("Error here", parsingError)
         }
-        }
+        })
         task.resume()
        
     
@@ -101,6 +109,7 @@ class PopularCell: UICollectionViewCell, UICollectionViewDelegate, UICollectionV
     func setup() {
         //ads setup
         let label = UILabel(frame: CGRect(x:0, y:0, width:150,height: 40))
+        label.font = UIFont.boldSystemFont(ofSize: 20.0)
         label.textAlignment = .left
         label.text = "인기맛집!"
         addSubview(label)
@@ -129,7 +138,13 @@ class PopularCell: UICollectionViewCell, UICollectionViewDelegate, UICollectionV
         labelName.text = restList[indexPath.row].name
         cell.contentView.addSubview(labelName)
         
-        if let imageURL = URL(string: restList[indexPath.row].logoURL),
+        
+        
+        
+        
+        DispatchQueue.global().async {
+        
+            if let imageURL = URL(string: self.restList[indexPath.row].logoURL),
             let data = try? Data(contentsOf: imageURL) {
             
             DispatchQueue.global().async {
@@ -143,7 +158,7 @@ class PopularCell: UICollectionViewCell, UICollectionViewDelegate, UICollectionV
             }
             
         }
-        
+        }
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -156,9 +171,11 @@ class PopularCell: UICollectionViewCell, UICollectionViewDelegate, UICollectionV
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let cell = collectionView.cellForItem(at: indexPath)
-        print("test")
+        print(restList[indexPath.row].id)
+        
     }
     
+
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -183,12 +200,9 @@ class PopularCell: UICollectionViewCell, UICollectionViewDelegate, UICollectionV
         }
         
         func setup() {
-            backgroundColor = .blue
             
             addSubview(imageView)
-            //            addSubview(labelName)
-            //            labelName.text = "testttt"
-            
+         
             imageView.setAnchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
             
         }
